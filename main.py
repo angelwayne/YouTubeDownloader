0005f23ccfde9b3876a219566fc6d5bf49becb6d
angelwayne <39891254+angelwayne@users.noreply.github.com>
2022-12-19 17:47:34 -0600

    # if you want set your file path you need something like this yotubeObject.download('/users/nameuser/YourPath')

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def Download(link):
     yotubeObject = yotubeObject.streams.get_highest_resolution()
     try:
         yotubeObject.download()
+        # if you want set your file path you need something like this yotubeObject.download('/users/nameuser/YourPath')
     except:
         print("Parece que ocurrio un error")
     print("El video se descargo con exito paps!!")
